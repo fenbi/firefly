7c4a4dbe3bfcaf02ac44fe3e5227c28c27b4b64b
liluo <i@liluo.org>
2013-03-07 22:17:02 +0800

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firefly::Application.config.secret_token = '2892429b3b9f4db1c5e5c9f1687252a5b7791489ab08374d5cd09c3056c8484f090a42442519c237262a6703b0f0e936064ccdb2c436fd49835c1a08ff6de6e1'
